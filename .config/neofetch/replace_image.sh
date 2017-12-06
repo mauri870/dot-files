0d13b7e289918bca0f462d22272630e7a3baa8e5
Mauri de Souza Nunes <mauri870@gmail.com>
2017-12-06 20:15:31 -0200

    Fix .gitignore

--- .config/neofetch/replace_image.sh
+++ .config/neofetch/replace_image.sh
@@ -4,4 +4,4 @@ WALLPAPER="$1"
 DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
 sed "s|image_source=\"auto\"|image_source=\"$WALLPAPER\"|" /etc/neofetch/config.conf | \
-sed "s|image_backend=\"ascii\"|image_backend=\"w3m\"|" > $DIR/config.conf
+    sed "s|image_backend=\"ascii\"|image_backend=\"w3m\"|" > $DIR/config.conf
