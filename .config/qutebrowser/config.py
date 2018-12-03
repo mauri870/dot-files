eb4f466cbd152abacc7f10ea4498c3c2ccaf0e8c
Mauri de Souza Nunes <mauri870@gmail.com>
2018-12-02 23:59:24 -0200

    Add qutebrowser config

---
+++ .config/qutebrowser/config.py
@@ -0,0 +1,10 @@
+
+c.url.searchengines['g'] = 'https://www.google.com.ua/search?q={}'
+c.url.searchengines['trans'] = 'https://translate.google.com/#auto/en/{}'
+c.url.searchengines['so'] = 'https://stackoverflow.com/search?q={}'
+c.url.searchengines['gh'] = 'https://github.com/search?q={}&type=Everything'
+c.url.searchengines['r'] = 'https://reddit.com/r/{}'
+c.url.searchengines['4'] = 'https://4chan.org/{}'
+c.url.searchengines['aw'] = 'https://wiki.archlinux.org?search={}'
+c.url.searchengines['tpb'] = 'https://thepiratebay.org/search/{}'
+
