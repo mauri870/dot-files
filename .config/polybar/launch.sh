14f4e4a2cdf47cc754b0797bd19b248970561ed6
Mauri de Souza Nunes <mauri870@gmail.com>
2019-11-17 20:56:15 -0300

    Display polybar in all connected monitors

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,13 +1,8 @@
-#!/usr/bin/env sh
+#!/bin/sh
 
-# Terminate already running bar instances
-killall -q polybar
-
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-# Launch topbar
-polybar topbar &
-
-
-echo "Bars launched..."
+pkill -f topbar
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload topbar &
+  done
+fi
