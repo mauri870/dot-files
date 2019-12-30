887f3ab4a6f83c30b5d756ddfc360c6f8bcad1da
Mauri de Souza Nunes <mauri870@gmail.com>
2019-12-30 10:49:35 -0300

    Qutebrowser: config.bind('<Alt-o>', 'open-editor')

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -45,3 +45,6 @@ c.tabs.background = True
 # `:open google qutebrowser`.
 # Type: Dict
 c.url.searchengines = {'DEFAULT': 'https://duckduckgo.com/?q={}', 'trans': 'https://translate.google.com/#auto/en/{}', 'so': 'https://stackoverflow.com/search?q={}', 'gh': 'https://github.com/search?q={}&type=Everything', 'r': 'https://reddit.com/r/{}', '4': 'https://4chan.org/{}', 'aw': 'https://wiki.archlinux.org?search={}', 'aur': 'https://aur.archlinux.org/packages/{}', 'tpb': 'https://thepiratebay.org/search/{}', 'godoc': 'https://godoc.org/{}', 'drs': 'https://docs.rs/{}', 'rs': 'https://doc.rust-lang.org/std/?search={}'}
+
+# Bindings for normal mode
+config.bind('<Alt-o>', 'open-editor')
