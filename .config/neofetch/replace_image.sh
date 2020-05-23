132232f5aacdab9bb19d7806819b2cc013aa166b
Mauri de Souza Nunes <mauri870@gmail.com>
2020-05-22 22:57:49 -0300

    Neofetch: Delete unused script to set backend

--- .config/neofetch/replace_image.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-WALLPAPER="$1"
-DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
-
-sed "s|image_source=\"auto\"|image_source=\"$WALLPAPER\"|" /etc/neofetch/config.conf | \
-    sed "s|image_backend=\"ascii\"|image_backend=\"w3m\"|" > $DIR/config.conf
